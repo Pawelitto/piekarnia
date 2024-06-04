e75ab525f83d033363d5931345e5a89271b6eb78
Paweł Pietras <pawel.pietras144@gmail.com>
2024-06-04 13:23:59 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+useHead({
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
+  htmlAttrs: {
+    lang: 'pl',
+  },
+});
+
+useSeoMeta({
+  ogImage: 'https://i.ibb.co/7z2wHbF/grafika4.jpg',
+  twitterImage: 'https://i.ibb.co/7z2wHbF/grafika4.jpg',
+  twitterCard: 'summary_large_image',
+  ogUrl: 'https://i.ibb.co/7z2wHbF/grafika4.jpg',
+  twitterTitle: 'Salon Fryzur - Wojciech Gudzik.',
+  twitterDescription: 'Odkryj swoją nową fryzurę już dziś',
+});
+</script>
+
+<template>
+  <Header />
+
+  <UMain>
+    <NuxtPage />
+  </UMain>
+
+  <Footer />
+  <UNotifications />
+</template>
---
+++ components/Kontakt.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="text-center md:grid md:grid-cols-2 md:gap-12 space-y-10 md:space-y-0"
+  >
+    <div>
+      <div
+        class="flex justify-center items-center mx-auto mb-4 w-10 h-10 bg-gray-100 rounded-lg dark:bg-gray-800 lg:h-16 lg:w-16"
+      >
+        <UIcon
+          name="i-heroicons-phone"
+          class="w-5 h-5 text-primary-600 lg:w-8 lg:h-8"
+          dynamic
+        />
+      </div>
+      <p class="mb-2 text-xl font-bold dark:text-white">Zadzwoń pod:</p>
+      <p class="mb-3 text-gray-500 dark:text-gray-400">
+        Chętnie odpowiemy na Twoje pytania i zapytania telefonicznie.
+      </p>
+      <NuxtLink
+        to="tel:+48661409195"
+        class="font-semibold text-primary-600 dark:text-primary-500"
+        >+48 661 409 195</NuxtLink
+      >
+    </div>
+    <div>
+      <div
+        class="flex justify-center items-center mx-auto mb-4 w-10 h-10 bg-gray-100 rounded-lg dark:bg-gray-800 lg:h-16 lg:w-16"
+      >
+        <UIcon
+          name="i-heroicons-map-pin"
+          class="w-5 h-5 text-primary-600 lg:w-8 lg:h-8"
+          dynamic
+        />
+      </div>
+      <p class="mb-3 text-xl font-bold dark:text-white">Lokalizacja</p>
+      <p class="mb-3 text-gray-500 dark:text-gray-400">
+        Sprawdź naszą lokalizację i odwiedź nas w dogodnym dla Ciebie terminie.
+      </p>
+      <NuxtLink
+        to="https://maps.app.goo.gl/zDaJYzqGc2pdGsfW6"
+        target="_blank"
+        class="inline-flex py-2 px-4 text-sm font-medium text-center rounded-lg border text-primary-600 border-primary-600 hover:text-white hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:border-primary-500 dark:text-primary-500 dark:hover:text-white dark:hover:bg-primary-600 dark:focus:ring-primary-800"
+        >Furgoła 183, 44-230 Czerwionka-Leszczyny</NuxtLink
+      >
+    </div>
+  </div>
+</template>
